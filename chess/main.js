a23dc2adf91869a57ea2be311fd355d0fbf1ff56
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2026-02-26 09:43:29 +0000

    place image in row 3/postion 3

--- chess/main.js
+++ chess/main.js
@@ -1,21 +1,43 @@
-// Create img
+// Create imgage
 const img = new Image(50, 50);
 img.src = "js.png";
 
+
 const container = document.getElementById("container");
 const table = document.getElementById("table");
+
 // Create component 5 cells in  1 row
 function addCellsInRow() {
-    let row = table.insertRow(-1);
-    for ( let i = 0; i < 5; i++){
-        let newCell = row.insertCell(-1);
-        newCell.textContent = "cell 21";
+  let row = table.insertRow(-1);
+  for (let i = 0; i < 5; i++) {
+    let newCell = row.insertCell(-1);
+    newCell.textContent = "cell 21";
+
+  /* Place Checkerboard in the 3 row, position 3
+     Row ${row.rowIndex} Cell ${i};
+     row.rowIndex == 3 + ${i} == 3 via boucle for i = 0 au début
+     Image cell postion 
+     If cell the 3 row, position 3 alors
+     Img include Cell include Row include Table
+  */
+    if (row.rowIndex == 2 && i == 2 ) {
+      newCell.textContent = " ";
+      newCell.appendChild(img);
+      
     }
+  }
 }
 
 // Repeat 5 times component addCellsInRow()
-for (let j = 0; j < 5; j++) {
+function finalCheckboard(){
+    for (let j = 0; j < 5; j++) {
     const newRow = addCellsInRow();
-
+   
+    }
 }
+// Display final checkboard
+finalCheckboard();
+
+
+
 
