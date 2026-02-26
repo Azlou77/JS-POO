9a9ce9a6a000e048aac60461f9a3be627fd25326
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2026-02-26 11:50:40 +0000

    add event keydown on button

--- chess/main.js
+++ chess/main.js
@@ -5,33 +5,36 @@ img.src = "js.png";
 
 const container = document.getElementById("container");
 const table = document.getElementById("table");
-
 // Create component 5 cells in  1 row
 function addCellsInRow() {
   let row = table.insertRow(-1);
   for (let i = 0; i < 5; i++) {
-    let newCell = row.insertCell(-1);
-    newCell.textContent = "cell 21";
+    let cell = row.insertCell(-1);
+    cell.textContent = "cell 21";
     let button = document.createElement("button");
-    
-  
-  
   
   /* Place Checkerboard in the 3 row, position 3
      Row ${row.rowIndex} Cell ${i};
      row.rowIndex == 3 + ${i} == 3 via boucle for i = 0 au début
      Image cell postion 
-     If cell the 3 row, position 3 alors
+     If cell the 3 row, position 3 then
      Img include Cell include Row include Table
   */
-    if (row.rowIndex == 2 && i == 2 ) {
-      newCell.textContent = " ";
-      button.appendChild(img);
-    }
-    newCell.appendChild(button);
-    
+  if (row.rowIndex == 2 && cell.cellIndex == 2) {
+    cell.textContent = " ";
+    button.appendChild(img);
+    button.addEventListener("keydown", (evt) => {
+      console.log(evt.key);
+    })
+  }
+
+  // Place button for event keydown
+  cell.appendChild(button);
   }
 }
+
+  
+
 
 // Repeat 5 times component addCellsInRow()
 function finalCheckboard(){
@@ -58,9 +61,9 @@ finalCheckboard();
 */
 // function defineCurrentPosition(){
 //   finalCheckboard();
-//   if (row.rowIndex == 2 && i == 2 && newCell.appendChild(img)){
-//     newCell.textContent = "This is current position";
+//   if (row.rowIndex == 2 && i == 2 && cell.appendChild(img)){
+//     cell.textContent = "This is current position";
 //   }
 // }
-// defineCurrentPosition();
+
 
