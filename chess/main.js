bdb12aec51be656cda830de64fb121ee2d1f1803
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2026-02-26 08:57:40 +0000

    Repeat 5 times component addCellsInRow()

--- chess/main.js
+++ chess/main.js
@@ -3,10 +3,19 @@ const img = new Image(50, 50);
 img.src = "js.png";
 
 const container = document.getElementById("container");
-const table = document.querySelector("table");
-let row = table.insertRow(-1);
-for ( let i = 0; i < 5; i++){
-    let newCell = row.insertCell(-1);
-    newCell.textContent = "cell 21";
-    
+const table = document.getElementById("table");
+// Create component 5 cells in  1 row
+function addCellsInRow() {
+    let row = table.insertRow(-1);
+    for ( let i = 0; i < 5; i++){
+        let newCell = row.insertCell(-1);
+        newCell.textContent = "cell 21";
+    }
 }
+
+// Repeat 5 times component addCellsInRow()
+for (let j = 0; j < 5; j++) {
+    const newRow = addCellsInRow();
+
+}
+
